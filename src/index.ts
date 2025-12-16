7d52cfa245591b3c5b10a7cb57fe964e7e5f7e11
Ewan Fabiani <ewfabia@emea.corpdir.net>
2025-12-16 14:49:44 +0100

    Imported react-native-get-random-values to work with react native

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
  * Signal Protocol React Library
  * Eine TypeScript-Implementierung des Signal Protocol für sichere End-to-End-Verschlüsselung
  */
+import 'react-native-get-random-values'
 
 export * from './pqxdh/index';
 export * from './double-ratchet/index';
