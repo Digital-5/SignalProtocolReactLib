61cb485c06e8e3cc555110851af8b213ecfdbd4f
Ewan Fabiani <85697017+EwanFabiani@users.noreply.github.com>
2026-06-03 14:18:39 +0200

    Fix bigint operations for signature validation

--- src/pqxdh/algorithms/XEdDSA.ts
+++ src/pqxdh/algorithms/XEdDSA.ts
@@ -164,7 +164,7 @@ export function xeddsa_verify(u:Uint8Array, M:Uint8Array, Signature:Uint8Array) 
     const R = Uint8ArrayToBigintLE(R_encoded_copy);
 
     // 1. Check if u and Signature are of correct length
-    if (u_converted >= CURVE25519_PARAMS.p || R >= 2**CURVE25519_PARAMS.pBits || s >= 2**CURVE25519_PARAMS.qBits) {
+    if (u_converted >= CURVE25519_PARAMS.p || R >= 2n ** BigInt(CURVE25519_PARAMS.pBits) || s >= 2n ** BigInt(CURVE25519_PARAMS.qBits)) {
         return false;
     }
 
